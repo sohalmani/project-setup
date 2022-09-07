f3dd8fc4ecc203d3010db9f9a14ede71cc88f6d2
Chander Shekhar <chander@webenertia.com>
2022-09-08 00:34:23 +0530

    testimonial blade html added

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,6 @@
 require(['modules/Car'], function(Car) {
   const car = new Car();
-  console.log(car.getOwner());
+  // console.log(car.getOwner());
  });
 
 // import Queue from './modules/Queue';
